f2381ff7484859ed606a20ae0b7ba4c7a147e7f7
wybecool <86187192+wybecool@users.noreply.github.com>
2025-02-12 18:31:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,6 +4,7 @@
 #sed -i '$a src-git smpackage https://github.com/Mike-qian/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git bypass https://github.com/tianiue/luci-app-bypass' feeds.conf.default
 #sed -i '1i src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
-sed -i '1i src-git kenzok81 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
-sed -i '1i src-git kenzok82 https://github.com/kenzok8/small-package.git' feeds.conf.default
-sed -i '1i src-git kenzok83 https://github.com/kenzok8/small.git' feeds.conf.default
+#sed -i '1i src-git kenzok81 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+#sed -i '1i src-git kenzok82 https://github.com/kenzok8/small-package.git' feeds.conf.default
+#sed -i '1i src-git kenzok83 https://github.com/kenzok8/small.git' feeds.conf.default
+sed -i '1i src-git lionel-package https://github.com/Lienol/openwrt-package.git' feeds.conf.default
