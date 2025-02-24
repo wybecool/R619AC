be6fb4c5eb66344674b4270248c5a8cf71cc6e57
wybecool <86187192+wybecool@users.noreply.github.com>
2025-02-24 18:24:37 +0800

    Update 23.05diy-2.sh

--- 23.05diy-2.sh
+++ 23.05diy-2.sh
@@ -31,10 +31,10 @@ curl -sfL https://github.com/leesuncom/R619AC/raw/master/patch/mosdns -o package
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # rm -rf feeds/kiddin9/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
- git clone https://github.com/sbwml/luci-app-mosdns -b v5 feeds/luci/applications/luci-app-mosdns
- git clone https://github.com/xuanranran/openwrt-package feeds/luci/applications/luci-app-adguardhome
+# git clone https://github.com/sbwml/luci-app-mosdns -b v5 feeds/luci/applications/luci-app-mosdns
+# git clone https://github.com/xuanranran/openwrt-package feeds/luci/applications/luci-app-adguardhome
 
-#find ./ | grep Makefile | grep adguardhome | xargs rm -f
-#find ./ | grep Makefile | grep mosdns | xargs rm -f
-#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-#git clone https://github.com/xuanranran/openwrt-package/luci-app-adguardhome package/adguardhome
+find ./ | grep Makefile | grep adguardhome | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/xuanranran/openwrt-package package/adguardhome
