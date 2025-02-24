50a3c87425890351afe05fcec2aa164907fb8805
wybecool <86187192+wybecool@users.noreply.github.com>
2025-02-24 17:42:58 +0800

    Update 23.05diy-2.sh

--- 23.05diy-2.sh
+++ 23.05diy-2.sh
@@ -33,7 +33,7 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # rm -rf feeds/kiddin9/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 # git clone https://github.com/sbwml/luci-app-mosdns -b v5 feeds/luci/applications/luci-app-mosdns
 
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep adguardhome | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/xuanranran/openwrt-package/luci-app-adguardhome package/adguardhome
